10b51fb6cc8aaad17e513cca2ffbb4593155713b
Everton Andrade <evertonfa7@gmail.com>
2021-12-01 16:57:12 -0300

    refactoring and format

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import cv2
 import numpy as np
 from time import *
 
+
 min_width = 80  # Largura minima do retangulo
 min_height = 80  # Altura minima do retangulo
 offset = 6  # Erro permitido entre pixel
 line_pos = 550  # Posição da linha de contagem
 delay = 60  # FPS do vídeo
 detec = []
-cars = trucks = 0
+cars = 0
 
 video_file = sys.argv[1]
 cap = cv2.VideoCapture(video_file)
 
-subtraction = (
-    cv2.createBackgroundSubtractorMOG2()
-)  # # Pega o fundo e subtrai do que está se movendo
+# Pega o fundo e subtrai do que está se movendo
+subtraction = cv2.createBackgroundSubtractorMOG2()
 
 
 def get_center(x, y, width, height):
@@ -38,37 +38,44 @@ def set_info(detec):
             print(f"Carros detectados até o momento: {cars}")
 
 
-def show_info(frame, dilatada):
+def show_info(frame, dilated):
     text = f"Carros: {cars}"
     cv2.putText(frame, text, (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
     cv2.imshow("Video Original", frame)
-    cv2.imshow("Detectar", dilatada)
+    cv2.imshow("Detectar", dilated)
 
 
 while True:
-    ret, frame = cap.read()  # Pega cada frame do vídeo
+    # Pega cada frame do vídeo
+    ret, frame = cap.read()
+
+    # Dá um delay entre cada processamento
     secs = float(1 / delay)
-    sleep(secs)  # Dá um delay entre cada processamento
-    grey = cv2.cvtColor(
-        frame, cv2.COLOR_BGR2GRAY
-    )  # Pega o frame e transforma para preto e branco
-    blur = cv2.GaussianBlur(
-        grey, (3, 3), 5
-    )  # Faz um blur para tentar remover as imperfeições da imagem
-    img_sub = subtraction.apply(blur)  # Faz a subtração da imagem aplicada no blur
-    dilat = cv2.dilate(img_sub, np.ones((5, 5)))  # "Engrossa" o que sobrou da subtração
-    kernel = cv2.getStructuringElement(
-        cv2.MORPH_ELLIPSE, (5, 5)
-    )  # Cria uma matriz 5x5, em que o formato da matriz entre 0 e 1 forma uma elipse dentro
-    dilated = cv2.morphologyEx(
-        dilat, cv2.MORPH_CLOSE, kernel
-    )  # Tenta preencher todos os "buracos" da imagem
+    sleep(secs)
+
+    # Pega o frame e transforma para preto e branco
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Faz um blur para tentar remover as imperfeições da imagem
+    blur = cv2.GaussianBlur(grey, (3, 3), 5)
+
+    # Faz a subtração da imagem aplicada no blur
+    img_sub = subtraction.apply(blur)
+
+    # "Engrossa" o que sobrou da subtração
+    dilat = cv2.dilate(img_sub, np.ones((5, 5)))
+
+    # Cria uma matriz 5x5, em que o formato da matriz entre 0 e 1 forma uma elipse dentro
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+    # Tenta preencher todos os "buracos" da imagem
+    dilated = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
     dilated = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
 
-    contorno, img = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    outline, img = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.line(frame, (25, line_pos), (1200, line_pos), (255, 127, 0), 3)
 
-    for (i, c) in enumerate(contorno):
+    for (i, c) in enumerate(outline):
         (x, y, w, h) = cv2.boundingRect(c)
         validate_outline = (w >= min_width) and (h >= min_height)
 
@@ -76,9 +83,9 @@ while True:
             continue
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        centro = get_center(x, y, w, h)
-        detec.append(centro)
-        cv2.circle(frame, centro, 4, (0, 0, 255), -1)
+        center = get_center(x, y, w, h)
+        detec.append(center)
+        cv2.circle(frame, center, 4, (0, 0, 255), -1)
 
     set_info(detec)
     show_info(frame, dilated)
